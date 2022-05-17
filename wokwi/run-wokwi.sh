2568b71abb0b224a2569f6454e16ed6070345637
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-05-17 11:29:24 +0000

    Update build and run wokwi task

--- wokwi/run-wokwi.sh
+++ wokwi/run-wokwi.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -e
-if [ "$USER" = "esp" ]; then
-path="/home/esp/workspace"
-else
-path="/workspace/esp-idf-devcontainer"
-fi
-pip3 install websockets==10.2
-echo Building and running Wokwi simulation for: $CURRENT_PROJECT
-cd $CURRENT_PROJECT
+# set -e
+# if [ "$USER" = "esp" ]; then
+#     path="/home/esp/workspace"
+# else
+#     path="/workspace/esp-idf-devcontainer"
+# fi
+# pip3 install websockets==10.2
+# echo Building and running Wokwi simulation for: $PROJECT_PATH
+# cd $PROJECT_PATH
 idf.py build
-cd $path
-python3 $path/wokwi/wokwi-server.py
+# cd $path
+python3 wokwi/wokwi-server.py
